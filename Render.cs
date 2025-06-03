9daedc96cce7282046d7a9143368986612f193b4
Taka005 <contact@takadev.jp>
2025-06-03 21:19:18 +0900

    C#スタイル設定とレンダリング機能の改善
    .editorconfigに暗黙のオブジェクト作成スタイルのルールを追加。
    Circle.csからCircleクラスと関連usingディレクティブを削除。
    Render.csでRenderクラスをFrameworkElementから継承し、FPS管理機能を追加。
    VisualControl.csに新しいVisualControlクラスを追加し、視覚要素の管理メソッドを実装。

--- Render.cs
+++ Render.cs
@@ -1,5 +1,58 @@
-﻿namespace PhysicsEngineRender{
-    public class Render{
+﻿using System.Diagnostics;
+using System.Windows;
 
+namespace PhysicsEngineCore{
+    public class Render : FrameworkElement{
+        public readonly VisualControl control = new VisualControl();
+        private Stopwatch stopwatch = new Stopwatch();
+        private TimeSpan _lastRenderTime = TimeSpan.Zero;
+        private TimeSpan _lastFpsUpdateTime = TimeSpan.Zero;
+        private int _frameCount = 0;
+        private double _fps = 0;
+        private int _targetFps;
+
+        public Render(int targetFps = 60){
+            this._targetFps = CheckTargetFps(targetFps);
+
+             this.stopwatch.Start();
+        }
+
+        public int fps{
+            get{
+                return (int)this._fps;
+            }
+        }
+
+         public int targetFps{
+            get{
+                return this._targetFps;
+            }
+            set{
+                this._targetFps = CheckTargetFps(value);
+            }
+        }
+
+        private void OnRendering(object? sender,EventArgs e){
+            TimeSpan currentTime = this.stopwatch.Elapsed;
+            double deltaTime = (currentTime - _lastRenderTime).TotalSeconds;
+
+            if(deltaTime < 1.0 / this._targetFps) return;
+
+            _frameCount++;
+
+            if((currentTime - _lastFpsUpdateTime).TotalSeconds >= 1){
+                _fps = _frameCount / (currentTime - _lastFpsUpdateTime).TotalSeconds;
+                _lastFpsUpdateTime = currentTime;
+                _frameCount = 0;
+            }
+
+            _lastRenderTime = currentTime;
+        }
+
+        private static int CheckTargetFps(int targetFps){
+            if(targetFps < 0) throw new Exception("目標FPSは0以上である必要があります");
+
+            return targetFps;
+        }
     }
 }
---
+++ VisualControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace PhysicsEngineCore{
+    public class VisualControl : FrameworkElement{
+        private readonly VisualCollection _visuals;
+
+        public VisualControl(){
+            _visuals = new VisualCollection(this);
+        }
+
+        public void AddVisual(Visual visual){
+            _visuals.Add(visual);
+        }
+
+        public void RemoveVisual(Visual visual){
+            if(_visuals.Contains(visual)){
+                _visuals.Remove(visual);
+            }
+        }
+
+        public void ClearVisuals(){
+            _visuals.Clear();
+        }
+
+        protected override int VisualChildrenCount{
+            get{
+               return _visuals.Count;
+            }
+        }
+
+        protected override Visual GetVisualChild(int index){
+            return _visuals[index];
+        }
+    }
+}
