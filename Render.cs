1c5f6387ebe3bbacfcb1e4c332369c5a302f04b9
Taka005 <contact@takadev.jp>
2025-06-04 16:18:45 +0900

    Render クラスのリファクタリングと統合
    Render.cs で `Render12` クラスを `Render` クラスに変更し、`VisualCollection` を管理する新しいプロパティや `UpdateAndDraw` メソッドを追加しました。これにより、描画の効率が向上し、オブジェクトの管理が容易になりました。
    
    Render1.cs から `Render` クラスを削除し、機能を `Render.cs` に統合しました。
    
    oldRender.cs では、`oldRender` クラスを追加し、基本的な描画機能を実装しました。このクラスは、`Render` クラスの前のバージョンとして機能します。

--- Render.cs
+++ Render.cs
@@ -1,58 +1,78 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using PhysicsEngineCore.Objects;
+using PhysicsEngineRender.Visuals;
+using System.Windows.Media;
 
 namespace PhysicsEngineRender{
-    public class Render12 : FrameworkElement{
-        public readonly VisualControl control = new VisualControl();
-        private Stopwatch stopwatch = new Stopwatch();
-        private TimeSpan _lastRenderTime = TimeSpan.Zero;
-        private TimeSpan _lastFpsUpdateTime = TimeSpan.Zero;
-        private int _frameCount = 0;
-        private double _fps = 0;
-        private int _targetFps;
+    public class Render {
+        public VisualCollection visuals;
 
-        public Render12(int targetFps = 60){
-            this._targetFps = CheckTargetFps(targetFps);
+        private readonly Dictionary<string, DrawingVisual> objectVisuals = [];
 
-             this.stopwatch.Start();
+        public Render() {
+            this.visuals = new VisualCollection(null);
         }
 
-        public int fps{
-            get{
-                return (int)this._fps;
+        /// <summary>
+        /// レンダリング対象のVisualCollectionを設定します
+        /// このメソッドはUIスレッドで呼び出される必要があります
+        /// </summary>
+        public void SetVisualCollection(VisualCollection visualCollection){
+            this.visuals = visualCollection;
+
+            foreach(var visual in this.objectVisuals.Values){
+                this.visuals.Add(visual);
             }
         }
 
-         public int targetFps{
-            get{
-                return this._targetFps;
-            }
-            set{
-                this._targetFps = CheckTargetFps(value);
-            }
+
+        /// <summary>
+        /// 物理エンジンのオブジェクトデータを受け取り、描画を更新します
+        /// このメソッドはUIスレッドで呼び出される必要があります
+        /// </summary>
+        /// <param name="objects">描画するIObjectのリスト</param>
+        public void UpdateAndDraw(List<IObject> objects) {
+            HashSet<string> currentObjectIds = [.. objects.Select(o => o.id)];
+            List<string>? visualsToRemove = this.objectVisuals.Keys.Where(id => !currentObjectIds.Contains(id)).ToList();
+
+            visualsToRemove.ForEach(id =>{
+                this.visuals.Remove(this.objectVisuals[id]);
+                this.objectVisuals.Remove(id);
+            });
+
+            objects.ForEach(obj => {
+                if(!this.objectVisuals.ContainsKey(obj.id)) {
+                    DrawingVisual? newVisual = this.CreateVisualForObject(obj);
+
+                    if(newVisual != null) {
+                        this.objectVisuals.Add(obj.id, newVisual);
+                        this.visuals.Add(newVisual);
+                    }
+                }
+
+                if(this.objectVisuals.TryGetValue(obj.id, out DrawingVisual? visual)) {
+                    if(visual is CircleVisual circleVisual) {
+                    }
+                    // 他のオブジェクトも追加
+                }
+            });
         }
 
-        public void OnRendering(object? sender,EventArgs e){
-            TimeSpan currentTime = this.stopwatch.Elapsed;
-            double deltaTime = (currentTime - this._lastRenderTime).TotalSeconds;
-
-            if(deltaTime < 1.0 / this._targetFps) return;
-
-            this._frameCount++;
-
-            if((currentTime - this._lastFpsUpdateTime).TotalSeconds >= 1){
-                this._fps = _frameCount / (currentTime - this._lastFpsUpdateTime).TotalSeconds;
-                this._lastFpsUpdateTime = currentTime;
-                this._frameCount = 0;
+        /// <summary>
+        /// オブジェクトの種類に基づいて適切なDrawingVisualを作成
+        /// </summary>
+        private DrawingVisual? CreateVisualForObject(IObject obj) {
+            if(obj is Circle circle) {
+                return new CircleVisual(circle);
             }
-
-            this._lastRenderTime = currentTime;
-        }
-
-        private static int CheckTargetFps(int targetFps){
-            if(targetFps < 0) throw new Exception("目標FPSは0以上である必要があります");
-
-            return targetFps;
+            // else if (obj is Rope rope)
+            // {
+            //     return new RopeVisual(rope);
+            // }
+            // else if (obj is Square square)
+            // {
+            //     return new SquareVisual(square);
+            // }
+            return null;
         }
     }
 }
---
+++ oldRender.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace PhysicsEngineRender{
+    public class oldRender : FrameworkElement{
+        public readonly VisualControl control = new VisualControl();
+        private Stopwatch stopwatch = new Stopwatch();
+        private TimeSpan _lastRenderTime = TimeSpan.Zero;
+        private TimeSpan _lastFpsUpdateTime = TimeSpan.Zero;
+        private int _frameCount = 0;
+        private double _fps = 0;
+        private int _targetFps;
+
+        public oldRender(int targetFps = 60){
+            this._targetFps = CheckTargetFps(targetFps);
+
+             this.stopwatch.Start();
+        }
+
+        public int fps{
+            get{
+                return (int)this._fps;
+            }
+        }
+
+         public int targetFps{
+            get{
+                return this._targetFps;
+            }
+            set{
+                this._targetFps = CheckTargetFps(value);
+            }
+        }
+
+        public void OnRendering(object? sender,EventArgs e){
+            TimeSpan currentTime = this.stopwatch.Elapsed;
+            double deltaTime = (currentTime - this._lastRenderTime).TotalSeconds;
+
+            if(deltaTime < 1.0 / this._targetFps) return;
+
+            this._frameCount++;
+
+            if((currentTime - this._lastFpsUpdateTime).TotalSeconds >= 1){
+                this._fps = _frameCount / (currentTime - this._lastFpsUpdateTime).TotalSeconds;
+                this._lastFpsUpdateTime = currentTime;
+                this._frameCount = 0;
+            }
+
+            this._lastRenderTime = currentTime;
+        }
+
+        private static int CheckTargetFps(int targetFps){
+            if(targetFps < 0) throw new Exception("目標FPSは0以上である必要があります");
+
+            return targetFps;
+        }
+    }
+}
