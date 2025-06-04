538e46c3948df2728df4164180f23d561a7aa114
Taka005 <contact@takadev.jp>
2025-06-04 15:43:40 +0900

    PhysicsEngineCoreの参照を追加し円を描画
    `PhysicsEngineRender.csproj`に`PhysicsEngineCore`への参照を追加し、`PhysicsEngineCore.dll`をプロジェクトに含めました。
    `CircleVisual.cs`にWPF関連の名前空間と`PhysicsEngineCore`のオブジェクトをインポートし、円を描画する`CircleVisual`クラスを実装しました。
    `UpdateDrawing`メソッドでUIスレッドでの描画を管理し、`Draw`メソッドで円の描画ロジックを定義しています。

---
+++ Visuals/CircleVisual.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using PhysicsEngineCore.Objects;
+using PhysicsEngineCore.Utils;
+
+namespace PhysicsEngineRender.Visuals{
+    class CircleVisual : DrawingVisual{
+        private readonly Circle objectData;
+        private Brush brush;
+        private Pen pen;
+        private Dispatcher dispatcher;
+
+        public CircleVisual(Circle objectData) {
+            this.objectData = objectData;
+            this.brush = Utility.ParseColor(objectData.color);
+            this.pen = new Pen(this.brush,1);
+            this.dispatcher = Application.Current.Dispatcher;
+        }
+
+        public void UpdateDrawing() {
+            if(this.dispatcher.Thread != Thread.CurrentThread){
+                this.dispatcher.InvokeAsync(()=> this.Draw());
+            }else{
+                this.Draw();
+            }
+        }
+
+        private void Draw() {
+            DrawingContext context = this.RenderOpen();
+
+            this.brush = Utility.ParseColor(this.objectData.color);
+            this.pen = new Pen(this.brush, 1);
+
+            context.DrawEllipse(
+                this.brush,
+                this.pen,
+                new Point(this.objectData.position.X, this.objectData.position.Y),
+                this.objectData.radius,
+                this.objectData.radius
+            );
+        }
+    }
+}
